845c895be46c21bd469f8aa02a3163fddfb8bd55
Pratik Daigavane <pratikdaigavane@mplgaming.com>
2022-09-29 15:39:18 +0530

    removed main

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package emoji_hash
 
 import (
 	"crypto/sha256"
@@ -9,9 +9,4 @@ func GenerateEmojiHash(data string) [32]byte {
 	sum := sha256.Sum256([]byte(data))
 	fmt.Printf("%x\n", sum)
 	return sum
-}
-
-func main() {
-	s := GenerateEmojiHash("Hello this is Pratik")
-	fmt.Println(s)
 }
